2ada9fdcd2a7f750edc4081bf48a65b5af76b65f
aiman-man <aim77495@gmail.com>
2026-09-21 16:23:12 +0800

    Delete submissions/aiman/mission-09-jobsheet-9 directory

--- submissions/aiman/mission-09-jobsheet-9/data.js
+++
@@ -1,5 +0,0 @@
-// data.js
-var filenames = ["100010.jpg", "100020.jpg", "100030.jpg"];
-var titles = ["Royal Portrait", "Noble Woman", "Historic Family"];
-var quantities = [1, 2, 3];
-var prices = [320.0, 155.0, 105.0];
